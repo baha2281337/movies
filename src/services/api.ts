16cef6624b3e1ea0569fd187f4892d32d5168936
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-04-09 09:51:46 +0600

    first commit

---
+++ src/services/api.ts
@@ -0,0 +1,15 @@
+const API = 'https://imdbapi.dev';
+
+export const searchMovies = async (query: string,limit = 50) => {
+    const res = await fetch('${API}/search/titles?query=${query}&limit=${limit}');
+    return res.json();
+
+};
+
+
+
+
+export const getMovieById = async (id: string) => {
+    const res = await fetch(`${API}/titles/${id}`);
+    return res.json();  
+}
